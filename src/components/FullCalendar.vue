ddcd591f367f3b0d21e03774975ba61b71b0c49b
Thibault Brazillier <thibault.brazillier@360learning.com>
2022-09-18 15:55:19 +0200

    feat: add config file

--- src/components/FullCalendar.vue
+++ src/components/FullCalendar.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
     import VueCal from "vue-cal";
     import "vue-cal/dist/vuecal.css";
+
+    import { ACTIVE_VIEW, DISABLED_VIEWS } from "./config";
 </script>
 
 <template>
     <VueCal
         class="full-calendar vuecal--green-theme"
+        :active-view="ACTIVE_VIEW"
+        :disable-views="DISABLED_VIEWS"    
     />
 </template>
 
---
+++ src/components/config.ts
@@ -0,0 +1,7 @@
+const ACTIVE_VIEW = "month";
+const DISABLED_VIEWS = ["years", "day"];
+
+export {
+    ACTIVE_VIEW,
+    DISABLED_VIEWS
+};
