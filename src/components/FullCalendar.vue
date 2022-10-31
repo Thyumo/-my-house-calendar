d05be954f3157314cf3864ebbf52d237fa5890f0
Thibault Brazillier <thibault.brazillier@360learning.com>
2022-10-31 12:08:01 +0100

    feat: create event on dbl click

--- src/components/FullCalendar.vue
+++ src/components/FullCalendar.vue
@@ -1,19 +1,34 @@
 <script setup lang="ts">
+    import { ref } from "vue";
+    // @ts-expect-error
     import VueCal from "vue-cal";
     import "vue-cal/dist/vuecal.css";
 
     import { ACTIVE_VIEW, DISABLED_VIEWS, EVENT_OPTIONS } from "./config";
+
+    const vueCal = ref<InstanceType<typeof VueCal | null>>(null);
+
+    function createFullDayEvent(event: Date) {
+        vueCal.value.createEvent(event, 0, { allDay: true, title: "Réservation", class: "day-event", content: "Réservé par: " })
+    };
+
+    defineExpose({
+        createFullDayEvent
+    });
 </script>
 
 <template>
     <VueCal
+        ref="vueCal"
         class="full-calendar vuecal--green-theme"
         :active-view="ACTIVE_VIEW"
         :disable-views="DISABLED_VIEWS"
+        :drag-to-create-event="false"
         :editable-events="EVENT_OPTIONS"
         locale="fr"
         :time="false"
         today-button
+        @cell-dblclick="createFullDayEvent($event)"
     >
         <template #today-button>
             <span class="today-button">
