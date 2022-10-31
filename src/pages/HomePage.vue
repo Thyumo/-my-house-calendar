2785a17928571e7eeff11c3659ddee8ce9c047a0
Thibault Brazillier <thibault.brazillier@360learning.com>
2022-10-31 12:34:16 +0100

    feat: add header

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -3,5 +3,17 @@
 </script>
 
 <template>
+    <div class="header">
+        Calendrier des réservations de Sainte-Vertus
+    </div>
+
     <FullCalendar />
 </template>
+
+<style scoped>
+    .header {
+        font-weight: 600;
+        font-size: 36px;
+        margin-bottom: 32px;
+    }
+</style>
