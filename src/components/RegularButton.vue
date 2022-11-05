30534753ae46048707d8aa8b6beb16a8f9388371
Thibault Brazillier <thibault.brazillier@360learning.com>
2022-11-05 15:07:53 +0100

    feat: create RegularButton component

---
+++ src/components/RegularButton.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+    interface Props {
+        text: string;
+        type: "main" | "secondary";
+    }
+
+    const props = defineProps<Props>();
+    defineEmits(["click"]);
+</script>
+
+<template>
+    <button :class="type" @click="$emit('click')">
+        {{ text }}
+    </button>
+</template>
+
+<style scoped>
+    .main {
+        background-color: #42b983;
+        color: #fff;
+    }
+
+    .main:hover {
+        border-color: #fff;
+    }
+
+    .secondary {
+        background-color: #e4f5ef;
+        color: #42b983;
+        outline: none;
+    }
+
+    .secondary:hover {
+        border-color: #42b983;
+    }
+</style>
