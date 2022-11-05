b86246e49517a13d29512de6f4230b31341dec95
Thibault Brazillier <thibault.brazillier@360learning.com>
2022-11-05 15:57:51 +0100

    feat: create InputWithLabel component

---
+++ src/baseComponents/InputWithLabel.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+    interface Props {
+        label: string;
+        value?: string;
+    }
+
+    defineProps<Props>();
+    defineEmits(["update:value"]);
+</script>
+
+<template>
+    <div class="input-with-label">
+        <label for="inputWithLabel">
+            {{ label }}
+        </label>
+
+        <input name="inputWithLabel" v-model="value" @change="$emit('update:value', value)" />
+    </div>
+</template>
+
+<style scoped>
+    .input-with-label {
+        display: flex;
+        flex-direction: column;
+        gap: 8px;
+    }
+</style>
