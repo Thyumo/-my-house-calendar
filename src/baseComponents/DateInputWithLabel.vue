02e67efabf8743c0c290878cfaa97880ee02cd08
Thibault Brazillier <thibault.brazillier@360learning.com>
2022-11-06 16:09:40 +0100

    feat: create and use date input

---
+++ src/baseComponents/DateInputWithLabel.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+    import { ref } from "vue";
+    import moment from "moment"
+
+    interface Props {
+        label: string;
+        date?: string;
+    }
+
+    defineProps<Props>();
+    const emits = defineEmits(["update:date"]);
+
+    const isDateInvalid = ref(false);
+
+    function updateDate(date: string | undefined) {
+        if (validateDate(date)) {
+            emits("update:date", date);
+        } else {
+            isDateInvalid.value = true
+        }
+    }
+
+    function validateDate(date: string | undefined) {
+        return moment(date, "DD/MM/YYYY").isValid();
+    }
+</script>
+
+<template>
+    <div class="input-with-label">
+        <label for="inputWithLabel">
+            {{ label }}
+        </label>
+
+        <input
+            class="date-input"
+            :class="{ 'error': isDateInvalid }"
+            name="inputWithLabel"
+            v-model="date"
+            @change="updateDate(date)"
+        />
+    </div>
+</template>
+
+<style scoped>
+    .input-with-label {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        gap: 8px;
+        flex: 1;
+    }
+
+    .date-input {
+        border: 0.75px solid;
+        border-radius: 4px;
+    }
+
+    .date-input:focus {
+        outline: none;
+        border: 1px solid #42b983;
+    }
+
+    .error {
+        border: 0.75px solid red;
+    }
+</style>
--- src/components/BookingPopupForm.vue
+++ src/components/BookingPopupForm.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
     import { ref } from "vue";
 
+    import DateInputWithLabel from "../baseComponents/DateInputWithLabel.vue";
     import InputWithLabel from '../baseComponents/InputWithLabel.vue';
 
     const formData = ref({
@@ -11,14 +12,28 @@
 
 <template>
     <div class="booking-popup-form">
-        <InputWithLabel label="Nom" :value="formData.name" @update:value="formData.name = $event" />
-    
-        <InputWithLabel label="Prénom" :value="formData.surname" @update:value="formData.surname = $event" />
+        <div class="form-section">
+            <InputWithLabel label="Nom" :value="formData.name" @update:value="formData.name = $event" />
+        
+            <InputWithLabel label="Prénom" :value="formData.surname" @update:value="formData.surname = $event" />
+        </div>
+
+        <div class="form-section">
+            <DateInputWithLabel label="Arrivée (JJ/MM/AAAA)" />
+
+            <DateInputWithLabel label="Départ (JJ/MM/AAAA)" />
+        </div>
     </div>
 </template>
 
 <style scoped>
     .booking-popup-form {
+        display: flex;
+        flex-direction: column;
+        gap: 16px;
+    }
+
+    .form-section {
         display: flex;
         gap: 8px;
     }
