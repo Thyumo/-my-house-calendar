ecd0bd35e83a156762b53dcfec19ced7a15d03aa
Thibault Brazillier <thibault.brazillier@360learning.com>
2023-05-04 14:19:02 +0200

    f formValidation

--- src/composables/formValidation.ts
+++ src/composables/formValidation.ts
@@ -3,19 +3,20 @@ import moment from "moment"
 
 import { useValidation } from "./validation";
 
+import type { Ref } from "vue";
 import type { BookingInputData } from "../types";
 
-export function useFormValidation(formData: BookingInputData) {
+export function useFormValidation(formData: Ref<BookingInputData>) {
     const { isDateValid, isEmpty } = useValidation();
 
-    const isFirstNameValid = computed(() => isEmpty(formData.firstName));
-    const isLastNameValid = computed(() => isEmpty(formData.lastName));
-    const isStartDateValid = computed(() => isDateValid(formData.startDate));
-    const isEndDateValid = computed(() => isDateValid(formData.endDate));
+    const isFirstNameValid = computed(() => isEmpty(formData.value.firstName));
+    const isLastNameValid = computed(() => isEmpty(formData.value.lastName));
+    const isStartDateValid = computed(() => isDateValid(formData.value.startDate));
+    const isEndDateValid = computed(() => isDateValid(formData.value.endDate));
 
     const isFormValid = computed(() => {
-        return isFirstNameValid && isLastNameValid && isStartDateValid && isEndDateValid;
+        return isFirstNameValid.value && isLastNameValid.value && isStartDateValid.value && isEndDateValid.value;
     });
 
-    return { isFormValid }
+    return { isFormValid };
 }
